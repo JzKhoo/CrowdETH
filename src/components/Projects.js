3df437e0092fecd9928ffd2b50cd4ec1c0d22fbf
Jing Zhi <e0773051@u.nus.edu>
2022-06-23 01:35:41 +0800

    Completed Projects Page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import Navbar2 from './Navbar2'
-import { } from './Projects.style'
+import { Container, MainSection, LeftSection, FeaturedProjectPoster, RightSection, ProjectCard, ProjectPoster, ProjectDetail } from './Projects.style'
+import Sample from '../images/Sample.jpeg'
 
 function Projects() {
   return (
     <>
         <Navbar2 />
-
+        <Container>
+          <h1>Projects</h1>  
+          <MainSection>
+            <LeftSection>
+              <h2>Featured Project</h2>
+              <FeaturedProjectPoster src={Sample} alt='Sample'></FeaturedProjectPoster>
+              <h3>Title</h3>
+              <p>Description</p>  
+            </LeftSection>
+            <RightSection>
+              <h2>More Projects</h2>
+              <ProjectCard>
+                <ProjectPoster src={Sample} alt='Sample'></ProjectPoster>
+                <ProjectDetail>
+                  <h4>Some Random Title</h4>
+                  <p>Some Random % Funded</p>
+                  <p>Some Random Owner</p>
+                </ProjectDetail>
+              </ProjectCard>
+            </RightSection>
+          </MainSection>
+        </Container> 
     </>
   )
 }
--- src/components/Projects.style.js
+++ src/components/Projects.style.js
@@ -1,3 +1,58 @@
 import styled from "styled-components";
 import BackgroundImage from '../images/Cosmic.jpeg'
 
+export const Container = styled.div`
+    background-image: url(${BackgroundImage});
+    width: 1915px;
+    height: 900px;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    h1 {
+        margin: 50px 0px 50px 100px;
+        font-size: 40px;
+    }
+`
+export const MainSection = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+`
+
+export const LeftSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 100px;
+    gap: 30px;
+`
+
+export const FeaturedProjectPoster = styled.img`
+    width: 60%;
+    height: 60%; 
+`
+
+export const RightSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+`
+
+export const ProjectCard = styled.div`
+    display: flex;
+`
+
+export const ProjectPoster = styled.img`
+    width: 10%;
+    height: 10%;
+    margin: 20px 50px 20px 0px;
+`
+
+export const ProjectDetail = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-top: 20px;
+`
