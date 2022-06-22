292e67a71491f6a9ea9797a6fe15af01140cb4f4
Jing Zhi <e0773051@u.nus.edu>
2022-06-22 17:52:37 +0800

    Completed Login Page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import BackgroundImage from '../images/Cosmic.jpeg'
 import GifImage from '../images/Logo.gif'
 import {Background, Description, StartButton, Gif} from './Hero.styles'
 
 function Hero() {
   return (
     <>
-      <Background src={BackgroundImage} alt='Background Image'>
-        <Description>
-          <h1>The Platform for Secure Crowdfunding</h1>
-          <p>Harness the unlimited potential of smart contracts and DeFi to invest in a business or start your new business venture.</p>
-          <StartButton>Get Started</StartButton>
-        </Description>
-        <Gif src={GifImage} alt='Gif'></Gif>
-      </Background>
+        <Background>
+            <Description>
+                <h1>The Platform for Secure Crowdfunding</h1>
+                <p>Harness the unlimited potential of smart contracts and DeFi to invest in a business or start your new business venture.</p>
+                <StartButton>Get Started</StartButton>
+            </Description>
+            <Gif src={GifImage} alt='Gif'></Gif>
+        </Background>
     </>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { Background, Password, Username, LoginButton } from './Login.styles'
 
 function Login() {
   return (
     <>
-      <h1>Hi</h1>
+      <Navbar />
+      <Background>
+        <h1>Login</h1>
+        <p>Start your venture journey with us!</p>
+        <Username></Username>
+        <Password></Password>
+        <LoginButton to='/'>Venture In!</LoginButton>
+      </Background>
     </>
   )
 }
