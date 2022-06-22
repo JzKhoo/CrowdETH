922b3fe2b20bc7d9716c06e9ae208571b73673e3
Zjinnnn <zongjin.2@gmail.com>
2022-06-23 00:34:20 +0800

    User Authentication Integration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, logInWithEmailAndPassword, signInWithGoogle } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Navbar from './Navbar';
 import { Background, Password, Username, LoginButton } from './Login.styles'
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user) navigate("/Landing");
+  }, [user, loading]);
+
   return (
     <>
       <Navbar />
       <Background>
         <h1>Login</h1>
         <p>Start your venture journey with us!</p>
-        <Username placeholder='Username'></Username>
-        <Password placeholder='Password'></Password>
-        <LoginButton to='/Landing'>Venture In!</LoginButton>
+        <Username 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Username'></Username>
+        <Password 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}        
+          placeholder='Password'></Password>
+        <LoginButton
+          onClick={() => logInWithEmailAndPassword(email, password)}
+          >Venture In!</LoginButton>
       </Background>
-    </>
+    </> 
   )
 }
 
