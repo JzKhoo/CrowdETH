d2fd982418d727266c095dd4d9716d467fbfdeac
Zjinnnn <zongjin.2@gmail.com>
2022-06-28 00:17:32 +0800

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './pages/HomePage/Home'
 import ScrollToTop from './ScrollToTop'
 
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const port = process.env.PORT || 8080 // Heroku will need the PORT environment variable
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.listen(port, () => console.log(`App is live on port ${port}!`))
+
 function App() {
   return (
     <>
