b2c19fb5fc5aa37aea174adb6dfcbd8009db7ca6
Zjinnnn <zongjin.2@gmail.com>
2022-06-28 01:04:26 +0800

    update???

--- src/App.js
+++ src/App.js
@@ -4,24 +4,6 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './pages/HomePage/Home'
 import ScrollToTop from './ScrollToTop'
 
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-
-const app = express()
-
-//here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
-// this * route is to serve project on different page routes except root /
-app.get(/.*/, function (req, res) {
- res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`app is listening on port: ${port}`)
-
 function App() {
   return (
     <>
